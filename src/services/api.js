a4a69930be8acf517bbe2b27c10cfaa78a215fc4
mwikyadevops-ke <mwikyadevops@gmail.com>
2025-11-28 14:29:30 +0300

    Refactor API base URL configuration to combine fallback values, ensuring proper functionality across different environments.

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL ||
-'http://localhost:8000/api'; 
-
-// 'https://shopifyapi.optusconnect.co.ke/api
+'http://localhost:8000/api'||
+'https://shopifyapi.optusconnect.co.ke/api';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
